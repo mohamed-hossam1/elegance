429207f9c219b224f79b781bf5f82ea91f0fd7fc
Mohamed Hossam <mohamedhossamv8@gmail.com>
2026-02-11 00:59:50 +0200

    Add reviews section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/landing/hero-section/HeroSection";
 import HowWorks from "@/components/landing/how-works-section/HowWorks";
 import ProjectsSection from "@/components/landing/projects-section/ProjectsSection";
+import Reviews from "@/components/landing/Reviews/Reviews";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <HeroSection />
         <ProjectsSection />
         <HowWorks />
+        <Reviews />
         {/* <Navbar />
         <HowItWorks />
         <TestimonialsSection />
--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,21 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'api.builder.io',
-        pathname: '/api/v1/image/assets/**',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**", 
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.builder.io",
+        pathname: "/api/v1/image/assets/**",
       },
     ],
   },
